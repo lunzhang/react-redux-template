b95f10b79661387d35b41dbf00acdb801568ce7c
lunzhang <lun.zhang1993@gmail.com>
2018-01-25 11:03:11 -0500

    Updated dependencies

--- src/app.js
+++ src/app.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, hashHistory } from 'react-router';
 import store from './store.js';
 import Main from './containers/main.js';
 
 // render app
 render(
   <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path="/" component={Main}>
-      </Route>
-    </Router>
+    <Main />
   </Provider>,
   document.getElementById('app'),
 );
